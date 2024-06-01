b05c4e575fc4a0f7ccbb7c8eea3fe1c2f371a7b7
RosaFraile <rosafraile@hotmail.com>
2024-06-01 15:45:49 +0200

    setup results component

---
+++ src/components/results.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+export default class Results  extends Component {
+  render() {
+    return (
+      <div>
+        <Logo />
+        <h1>Results</h1>
+      </div>
+    );
+  }
+}
