2976672c9c6deb6d34a19ef274d05fb02c28c74e
RosaFraile <rosafraile@hotmail.com>
2024-06-02 21:49:39 +0200

    used axios to perform get request to fetch recents posts and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,19 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        // perform our request in here.
-        console.log("hello");
+        axios.get('https://rosafraile.devcamp.space/portfolio/portfolio_items')
+            .then(response => {
+                console.log("response", response.data.portfolio_items);
+                dispatch({
+                    type: SET_RECENT_POSTS,
+                    payload: response.data.portfolio_items
+                })
+            })
+            .catch(error => {
+                console.log("fetccRecentPosts error", error)
+            })
     }
 }
