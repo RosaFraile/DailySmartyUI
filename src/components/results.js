fbcbb10914298cad15b297c64f39527e8d3d1e48
RosaFraile <rosafraile@hotmail.com>
2024-06-04 18:33:57 +0200

    modified Logo component to take in a custo size via props

--- src/components/results.js
+++ src/components/results.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+
 import Logo from './logo';
+import SearchBar from './search-bar';
 
 export default class Results  extends Component {
   render() {
     return (
       <div>
-        <Logo />
-        <h1>Results</h1>
+        <Logo size={55} />
+        <SearchBar />
       </div>
     );
   }
